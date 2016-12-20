7aeaf8c103fdfc9f9ee31d16231a0e4f938442ca
Jeffrey Estrada <jestrada@wpi.edu>
2016-12-20 13:45:59 -0500

    Created the calculator

--- Directory.java
+++ Directory.java
@@ -56,6 +56,38 @@ public class Directory{
 		}		
 	}
 
+	// Now to handle all User interactions with Directory
 	
+	public void calculate(){
+		Scanner sc = new Scanner(System.in);
+		int total = 0;
+		System.out.println("Give input for each ticket as follows:");
+		System.out.println("Enter ONLY final ticket number if no new blocks were set");
+		System.out.println("Enter ONLY starting and ending ticket number if there was a new block w/ equal price");
+		System.out.println("Enter only starting,ending, and new price if there was a new block w/ new price");
+		for (int i = 0; i < this.directory.size(); i++){
+			System.out.print("Input for Ticket " + (i + 1) + ": ");
+			String input = sc.nextLine();
+			String[] info = input.split("\\s+");
+
+			if (info.length == 3){
+				total += directory.get(i).update(Integer.parseInt(info[0]), Integer.parseInt(info[1]), Integer.parseInt(info[2]));
+				System.out.println("Earnings: $" + total);
+			}
+			else if (info.length == 2){
+				total += directory.get(i).update(Integer.parseInt(info[0]), Integer.parseInt(info[1]));
+				System.out.println("Earnings: $" + total);
+			}
+			else if (info.length == 1){
+				total += directory.get(i).update(Integer.parseInt(info[0]));
+				System.out.println("Earnings: $" + total);
+			}
+			else{
+				System.out.println("Invalid Entry. Try Again.");
+				i--;
+			}
+			
+		}
+	}	
 
 }
--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
 	public static void main(String[] args) throws IOException{
 		Ticket test = new Ticket(1,1,1);
 		Directory ourDirect = new Directory("Data.txt");
-		ourDirect.print_all();
+		ourDirect.calculate();
 		System.out.println(test.get_tick());
 	}
 }
